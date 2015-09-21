3db3d66bf1a87d55f50483758a2646024cbd945a
mansoa <mansoa4@mcmaster.ca>
2015-09-20 20:58:47 -0400

    Assignment1
    Assignment1 for 2S03

---
+++ HWK1_mansoa4.java
@@ -0,0 +1,16 @@
+/*
+Name: Aamir Mansoor
+MacID: mansoa4
+Student Number: 1406581
+Description: This file takes in three arguments and prints them out in reverse order. 
+*/
+
+public class HWK1_mansoa4{
+
+	public static void main(String[] args) {
+		
+		System.out.println(args[2] + " " + args[1] + " " + args[0]);
+
+	}
+
+}
